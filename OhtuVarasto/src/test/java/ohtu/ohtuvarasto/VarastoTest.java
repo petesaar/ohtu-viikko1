c60af8e95d849868553ef917a0c5f180b0888588
petesaar <pete.saarela@pp.inet.fi>
2015-03-14 20:45:49 +0200

    saatoa4

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -27,7 +27,7 @@ public class VarastoTest {
 
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
-        assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
+        assertEquals(101, varasto.getTilavuus(), vertailuTarkkuus);
     }
 
     @Test
